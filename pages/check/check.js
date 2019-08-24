09f13719ef2186b342a31fca756061c7d4397272
HarleyCody <www.codewzy@qq.com>
2019-08-23 21:11:08 -0400

    check Page completed

--- pages/check/check.js
+++ pages/check/check.js
@@ -17,7 +17,6 @@ Page({
    服务评价函数，动态改变Radios
   */
   chooseService: function (res) {
-    console.log("选中的标签：" + res.detail.value);
     var arrs = this.data.servicesArray;
     var that = this;
     var tipRate = 0;
@@ -25,13 +24,13 @@ Page({
       if (arrs[x].name == res.detail.value) {
         arrs[x].checked = true;
         tipRate = arrs[x].val;
-        console.log("小费百分比为:" + tipRate);
       } else {
         arrs[x].checked = false;
       }
     }
     that.setData({
       servicesArray: arrs,
+      defualtTipRate:tipRate,
       staticTipRate: tipRate,
       TRValue:"",
       isTipRate:false
@@ -41,22 +40,95 @@ Page({
   /*
   获取账单金额
   */
-  subtotalling:function(res){
-    console.log("账单金额获取前sub" + this.data.sub);
+  newSubtotal:function(res){
     this.validateSub(res.detail.value);
     if(this.data.isSubtotal || res.detail.value.trim().length ==0 ){
       this.setData({
         subtotal: res.detail.value,
       });
       this.calWithSubtotal();
-      console.log("账单金额" + this.data.subtotal);
-      console.log("账单金额获取后sub" + this.data.isSubtotal);
-    }
-    else{
+    }else{
       wx.showToast({
         title: 'Invalid Subtotal',
         icon: 'none',
         duration:800,
+      })
+    }
+  },
+  /*获取新 Tip*/
+  newTip: function (res) {
+    this.validateTip(res.detail.value);
+    if (this.data.isRound) {
+      wx.showToast({
+        title: 'Turn off Reserve to proceed',
+        icon: 'none',
+        duration: 800,
+      });
+      return;
+    }
+    if (this.data.isTip) {
+      this.setData({
+        staticTip: res.detail.value,
+      })
+      console.log("小费: " + this.data.staticTipRate);
+      this.calWithTip();
+    } else {
+      wx.showToast({
+        title: 'Invalid TipRate',
+        icon: 'none',
+        duration: 800,
+      })
+    }
+  },
+
+  /*获取新 Total*/
+  newTotal: function (res) {
+    this.validateTotal(res.detail.value);
+    if (this.data.isRound || res.detail.value.trim().length == 0) {
+      wx.showToast({
+        title: 'Turn off Reserve to proceed',
+        icon: 'none',
+        duration: 800,
+      });
+      return;
+    }
+    if (this.data.isTotal || res.detail.value.trim().length == 0) {
+      this.setData({
+        staticTotal: Math.floor(res.detail.value * 100) / 100,
+      });
+      console.log("合计:" + this.data.staticTotal);
+      this.calWithTotal();
+    } else {
+      wx.showToast({
+        title: 'Invalid TipRate',
+        icon: 'none',
+        duration: 800,
+      })
+    }
+  },
+
+  /*获取新 Tip Rate*/
+  newTipRate: function (res) {
+    this.validateTipRate(res.detail.value);
+    if(this.data.isRound){
+      wx.showToast({
+        title: 'Turn off Reserve to proceed',
+        icon: 'none',
+        duration: 800,
+      });
+      return;
+    }
+    if (this.data.isTipRate || res.detail.value.trim().length == 0) {
+      this.setData({
+        staticTipRate: Math.floor(res.detail.value * 100)/100,
+      });
+      console.log("小费率" + this.data.staticTipRate);
+      this.calWithTipRate();
+    } else {
+      wx.showToast({
+        title: 'Invalid TipRate',
+        icon: 'none',
+        duration: 800,
       })
     }
   },
@@ -73,15 +145,13 @@ Page({
       this.setData({
         round:"Yes"
       })
-    }
-    else{
+    }else{
       this.setData({
         round: "Nope"
       })
     }
     if(this.data.isSubtotal){
       if (this.data.isTipRate){
-        console.log("calWithTR");
         this.calWithTipRate();
       }else if(this.data.isTotal){
         this.calWithTotal();
@@ -93,38 +163,75 @@ Page({
     }
   },
 
-  newTipRate:function(res){
-    this.validateTipRate(res.detail.value);
-    if (this.data.isTipRate) {
+  /************************************************************Validate block***********************************************************************/
+
+  validateSub: function (subtotal) {
+    if (!isNaN(Number(subtotal)) && Number(subtotal) > 0) {
       this.setData({
-        staticTipRate: res.detail.value,
-      });
-      console.log("小费率" + this.data.staticTipRate);
-      this.calWithTipRate();
+        isSubtotal: true
+      })
     }
     else {
-      wx.showToast({
-        title: 'Invalid TipRate',
-        icon: 'none',
-        duration: 800,
+      this.setData({
+        isSubtotal: false
+      })
+    }
+  },
+
+  validateTipRate: function (tipRate) {
+    if (!isNaN(Number(tipRate)) && Number(tipRate) > 0) {
+      this.setData({
+        isTipRate: true
+      })
+    }
+    else {
+      this.setData({
+        isTipRate: false
+      })
+    }
+  },
+
+  validateTip: function (t) {
+    if (!isNaN(Number(t)) && Number(t) > 0) {
+      this.setData({
+        isTip: true
+      })
+    }
+    else {
+      this.setData({
+        isTip: false
+      })
+    }
+  },
+
+  validateTotal: function (total) {
+    if (!isNaN(Number(total)) && Number(total) > 0) {
+      this.setData({
+        isTotal: true
+      })
+    }
+    else {
+      this.setData({
+        isTotal: false
       })
     }
   },
 
   /*********************************************************************caculate block***************************************************************/
-  /*
-  计算最终账单
-  */
+  /**
+   *计算最终账单
+   */
   calWithSubtotal:function(){
+    console.log("Running with Subtotal");
     if(this.data.isSubtotal){
       var subtotal = Number(this.data.subtotal);
-      var tipRate = Number(this.data.staticTipRate);
+      var tipRate = Number(this.data.defualtTipRate);
       var t = subtotal + subtotal * tipRate / 100;
       var tr = (t-subtotal)/subtotal * 100;
       if (this.data.isRound) {
         this.setData({
           total: parseInt(t),
-          tipRate: tr,
+          tipRate: parseInt(t-subtotal) * 100 /subtotal,
           tip: parseInt(t-subtotal),
         })
       }else{
@@ -145,23 +252,90 @@ Page({
     }
   },
 
+  /* 通过 小费 计算 */
+  calWithTip: function () {
+    console.log("Running with Tip");
+    if (this.data.isTip) {
+      this.setData({
+        TRValue: "",
+        TotValue: ""
+      });
+      var ti = Number(this.data.staticTip);
+      var subtotal = Number(this.data.subtotal);
+      var tr = ti / subtotal;
+      var to = subtotal + ti;
+      if (this.data.isRound) {
+        ti = Math.floor((parseInt(to) - subtotal) * 100) / 100;
+        this.setData({
+          total: parseInt(to),
+          TValue: ti,
+          tipRate: (ti) * 100 / subtotal
+        });
+      } else {
+        this.setData({
+          total: Math.floor(to * 100) / 100,
+          tipRate: Math.floor(((to - subtotal) / subtotal) * 10000) / 100,
+        });
+      }
+    }
+    else {
+      this.calWithSubtotal();
+    }
+  },
+
+  /*通过 总额 计算*/
+  calWithTotal: function () {
+    console.log("Running with Total");
+    this.setData({
+      TValue: "",
+      TRValue: ""
+    });
+    var to = Number(this.data.staticTotal);
+    var sub = Number(this.data.subtotal);
+    var ti = to - sub;
+    var tr = ti*100 / sub;
+    if(this.data.isTotal){
+      if (this.data.isRound) {
+        ti = parseInt(ti);
+        this.setData({
+          tip: ti,
+          TotValue: Math.floor((sub + ti) * 100) / 100,
+          tipRate: tr,
+        });
+      } else {
+        this.setData({
+          tip:Math.floor(ti*100)/100,
+          tipRate: tr,
+        })
+      }
+    }
+    else {
+      this.calWithSubtotal();
+    }
+  },
+
+  /* 通过 小费率 计算 */
   calWithTipRate:function(){
+    
+    console.log("Running with TipRate");
     if(this.data.isTipRate){
-      console.log("staticTipRate: " + this.data.staticTipRate);
+      this.setData({
+        TValue: "",
+        TotValue: ""
+      });
       var subtotal = Number(this.data.subtotal);
       var tipRate = Number(this.data.staticTipRate);
-      console.log("tipRate: " + tipRate);
       var t = subtotal + subtotal * tipRate / 100;
       var ti = (t - subtotal);
       
       if(this.data.isRound){
         this.setData({
           total: parseInt(t),
-          TRValue: (parseInt(t)-subtotal) * 100/subtotal,
+          TRValue: 100*ti/subtotal,
           tipRate:this.data.staticTipRate,
-          tip: parseInt(t - subtotal)
+          tip: (parseInt(t) - subtotal).toFixed(2),
         })
-        console.log("取整后小费："+ this.data.TRValue);
+        console.log("Tip 未保留两位小数"+ this.data.tip);
       }else{
         this.setData({
           total: Math.floor(t * 100) / 100,
@@ -171,41 +345,11 @@ Page({
       }
     }
     else{
-      this.setData({
-        total: "",
-        tipRate: "",
-        tip: ""
-      });
+      this.calWithSubtotal();
     }
   },
 
-  /************************************************************Validate block***********************************************************************/
-
-  validateSub: function (subtotal) {
-    if (!isNaN(Number(subtotal)) && Number(subtotal) > 0) {
-      this.setData({
-        isSubtotal: true
-      })
-    }
-    else {
-      this.setData({
-        isSubtotal: false
-      })
-    }
-  },
-
-  validateTipRate: function (tipRate){
-    if (!isNaN(Number(tipRate)) && Number(tipRate) > 0) {
-      this.setData({
-        isTipRate: true
-      })
-    }
-    else {
-      this.setData({
-        isTipRate: false
-      })
-    }
-  },
+  /*****************************************************************Others      ***************************************************************************/
 
   /*****************************************************************Life Cycle***********************************************************************/
   /**
@@ -215,6 +359,7 @@ Page({
     var time = util.formatTime(new Date());
     this.setData({
       time : time,
+      defualtTipRate:15,
       staticTipRate: 15
     })
   },
